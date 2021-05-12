f2e22cec701a9f7d0428b34fc4af7beb588db83c
Aksel Davis <33839009+akseldavis@users.noreply.github.com>
2021-05-12 00:36:06 -0400

    Ruby rename file

--- pdf-download.rb
+++ pdf-download.rb
@@ -1,4 +1,7 @@
+require 'fileutils'
 dest_path = '/tmp/output/pdfs/fullsite-sidebar/'
 endpoint = 'https://docs.netapp.com/us-en/occm/pdfs/fullsite-sidebar/Cloud_Manager_documentation.pdf'
-pdf_path = "#{dest_path}Cloud_Manager_のドキュメント.pdf"
-%x[ curl -o #{pdf_path} #{endpoint} ] unless File.exists?("#{pdf_path}")
+ja_path = "#{dest_path}Cloud_Manager_のドキュメント.pdf"
+en_path = "#{dest_path}occm.pdf"
+%x[ curl -o #{en_path} #{endpoint} ] unless File.exists?("#{en_path}")
+FileUtils.mv(en_path.to_s, ja_path.to_s)
